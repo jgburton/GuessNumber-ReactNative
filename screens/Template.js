216b16895d21820a0021cfad378492f2640cc603
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2019-10-31 14:50:19 +0000

    added Start Game Screen component, began to style...

---
+++ screens/Template.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const StartGameScreen = props => {
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default StartGameScreen;
