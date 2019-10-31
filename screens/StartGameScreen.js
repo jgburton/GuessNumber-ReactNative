6a40c92275b8b539af1785624792c9126d0cbaa9
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2019-10-31 15:05:27 +0000

    Styling input container

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -30,7 +30,17 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+         //ios only
+        shadowColor: 'black',
+        shadowOffset: {width:0, height:2},
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        //ios only
+        elevation: 8, //android only
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
     },
     buttonContainer: {
         flexDirection: 'row',
